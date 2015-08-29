b7f13c11b95ea5d4b606cdabe535fa348b1f4f75
IlTempe <teosoft80@gmail.com>
2015-08-29 23:32:17 +0200

    Update remove_blank.php

--- remove_blank.php
+++ remove_blank.php
@@ -2,27 +2,11 @@
 
 //Rimuove righe vuote da un file di testo.
 
-file_put_contents('1new.log',
+file_put_contents('newfile.log',
     preg_replace(
         '~[\r\n]+~',
         "\r\n",
-        trim(file_get_contents('1.log'))
-    )
-);
-
-file_put_contents('2new.log',
-    preg_replace(
-        '~[\r\n]+~',
-        "\r\n",
-        trim(file_get_contents('2.log'))
-    )
-);
-
-file_put_contents('3new.log',
-    preg_replace(
-        '~[\r\n]+~',
-        "\r\n",
-        trim(file_get_contents('3.log'))
+        trim(file_get_contents('file.log'))
     )
 );
 
